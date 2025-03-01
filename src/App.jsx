8889a39a359b65853a0b08ffcf7a485789ec9f4e
anishpatel691 <youtubanish@gmail.com>
2025-03-01 23:45:28 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import YouTubeSEOAnalyzer from "./components/YT/YouTubeSEOAnalyzer";
+
+export default function App() {
+    return (
+        <>
+        <YouTubeSEOAnalyzer/>
+        </>
+    )
+}
